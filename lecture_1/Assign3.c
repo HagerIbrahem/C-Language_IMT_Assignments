b947f92b61b5eabc421e95773666a287f8dd5844
HagerIbrahem <118101867+HagerIbrahem@users.noreply.github.com>
2022-12-03 13:02:26 +0200

    Add files via upload

---
+++ lecture_1/Assign3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{  
+  int a,b;
+  printf("please Enter number a:");
+  scanf("%d",&a);
+  
+  printf("please Enter number b:");
+  scanf(" %d",&b);
+   
+  printf("a+b= %d \n", a+b);
+  printf("a-b= %d \n", a-b);
+  printf("a&b= %d \n", a&b);
+  printf("a|b= %d \n", a|b);
+  printf("a^b= %d \n", a^b);
+ 
+ 
+}
